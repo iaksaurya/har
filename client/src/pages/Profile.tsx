4066de705e7b37b4a9c25e8b1f8c53e8486e0426
Abhimanyu Kumar <abhimanyukumar2090@gmail.com>
2024-10-16 02:15:00 +0530

    profile error debug

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
-import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "../lib/firebase";
 import { store } from "../lib/store";
 import Container from "../ui/Container";
@@ -7,22 +7,45 @@ import Registration from "../ui/Registration";
 import UserInfo from "../ui/UserInfo";
 import Loading from "../ui/Loading";
 
-const Profile = () => {
-  const { currentUser, getUserInfo, isLoading } = store();
+// Update UserType to make 'address' optional
+type UserType = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  avatar: string;
+  id: string;
+  address?: string; // Make 'address' optional
+};
+
+const Profile: React.FC = () => {
+  // Update store return types
+  const { currentUser, getUserInfo, isLoading } = store() as {
+    currentUser: UserType | null;
+    getUserInfo: (uid: string) => void;
+    isLoading: boolean;
+  };
+
   useEffect(() => {
-    const unSub = onAuthStateChanged(auth, (user) => {
-      getUserInfo(user?.uid);
+    const unSub = onAuthStateChanged(auth, (user: User | null) => {
+      if (user?.uid) {
+        getUserInfo(user.uid);
+      }
     });
     return () => {
       unSub();
     };
   }, [getUserInfo]);
+
   return (
     <Container>
-      {currentUser ? <UserInfo currentUser={currentUser} /> : <Registration />}
-
+      {currentUser ? (
+        <UserInfo currentUser={currentUser} />
+      ) : (
+        <Registration />
+      )}
       {isLoading && <Loading />}
     </Container>
   );
 };
+
 export default Profile;
