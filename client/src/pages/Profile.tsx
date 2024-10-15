7dcc03f632bf0aa04380a1f58d62e70736a54a03
Abhimanyu Kumar <abhimanyukumar2090@gmail.com>
2024-10-16 01:57:34 +0530

    error debug

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
+import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
-import { onAuthStateChanged, User } from "firebase/auth"; // Type import for User
 import { auth } from "../lib/firebase";
 import { store } from "../lib/store";
 import Container from "../ui/Container";
@@ -7,23 +7,20 @@ import Registration from "../ui/Registration";
 import UserInfo from "../ui/UserInfo";
 import Loading from "../ui/Loading";
 
-const Profile: React.FC = () => {
+const Profile = () => {
   const { currentUser, getUserInfo, isLoading } = store();
-
   useEffect(() => {
-    const unSub = onAuthStateChanged(auth, (user: User | null) => {
-      if (user?.uid) {
-        getUserInfo(user.uid);
-      }
+    const unSub = onAuthStateChanged(auth, (user) => {
+      getUserInfo(user?.uid);
     });
     return () => {
       unSub();
     };
   }, [getUserInfo]);
-
   return (
     <Container>
       {currentUser ? <UserInfo currentUser={currentUser} /> : <Registration />}
+
       {isLoading && <Loading />}
     </Container>
   );
